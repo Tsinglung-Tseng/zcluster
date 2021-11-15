24e3c413a85934f2652584b427612fa44e186b74
Tsinglung-Tseng <tsinglung.tseng@gmail.com>
2021-11-15 15:54:26 +0800

    debug

--- controller/ln-controller.sh
+++ controller/ln-controller.sh
@@ -1,5 +1,3 @@
 rm /etc/nixos/configuration.nix
-rm /etc/nixos/hardware-configuration.nix
 
 ln -s $(pwd)/configuration.nix /etc/nixos/configuration.nix
-ln -s $(pwd)/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
